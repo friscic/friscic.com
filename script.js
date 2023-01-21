575c1f426bf4182d81e5d6cb7e9c5ec21cc17e77
friscic <mfriscic@gmail.com>
2023-01-21 21:51:06 +0100

    s

--- script.js
+++ script.js
@@ -1,99 +1,123 @@
-const lines = document.getElementById("lines");
-const input = document.getElementById("input");
-const cursor = document.getElementById("cursor");
+class Console {
+    maxCharInput = 10;
+    content = {};
+    lines = document.getElementById("lines");
+    input = document.getElementById("input");
+    cursor = document.getElementById("cursor");
 
-let content = {};
-fetch("./data.json")
-    .then((response) => response.json())
-    .then((json) => {
-        content = json;
-    });
-
-const urlParams = new URLSearchParams(window.location.search);
-const page = urlParams.get("page");
-if (page && !isNaN(page)) {
-    inputValidator(page);
-}
-
-function appendInput(char) {
-    input.innerText += char;
-    cursor.value = "";
-}
-
-function newLine(options = {}) {
-    const line = document.createElement("div");
-    if (options.text) {
-        line.innerText = options.text;
+    constructor() {
+        this.checkUriParam();
+        this.init();
     }
-    line.className += options.highlight ? " highlight" : "";
-    if (options.href) {
-        let link = document.createElement("a");
-        link.href = options.href;
-        link.target = "_blank";
-        line.append(link);
+
+    init() {
+        fetch("./data.json")
+            .then((response) => response.json())
+            .then((json) => {
+                this.content = json;
+            });
+
+        document.addEventListener("keydown", (event) => {
+            switch (event.code) {
+                case "Backspace": {
+                    this.input.innerText = input.innerText.substr(
+                        0,
+                        this.input.innerText.length - 1
+                    );
+                    break;
+                }
+                default:
+                    break;
+            }
+        });
+
+        document.addEventListener("keypress", (event) => {
+            switch (event.code) {
+                case "Enter": {
+                    this.newLine({ text: `~/${this.input.innerText} ` });
+                    this.inputValidator();
+                    this.input.innerText = "";
+                    break;
+                }
+                default:
+                    this.appendInput(event.key);
+                    event.preventDefault();
+                    break;
+            }
+        });
+
+        document.addEventListener("input", (event) => {
+            if (event.data) {
+                this.appendInput(event.data);
+            }
+        });
     }
-    if (options.command) {
-        switch (options.command) {
-            case "C": { // TODO
-                lines.innerText = "";
+
+    checkUriParam() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const page = urlParams.get("page");
+        if (page && !isNaN(page)) {
+            this.inputValidator(page);
+        }
+    }
+
+    appendInput(char) {
+        if (this.input.innerText.length <= this.maxCharInput) {
+            this.input.innerText += char;
+        }
+        this.cursor.value = "";
+    }
+
+    newLine(options = {}) {
+        const line = document.createElement("div");
+        line.innerText = options.text
+            ? options.text.replace("%I", this.input.innerText)
+            : line.innerText;
+        line.className += options.highlight ? " highlight" : "";
+
+        if (options.href) {
+            let link = document.createElement("a");
+            link.href = options.href;
+            link.target = "_blank";
+            line.append(link);
+        }
+
+        if (options.command) {
+            switch (options.command) {
+                case "C": {
+                    // TODO
+                    lines.innerText = "";
+                }
             }
         }
-    }
-    lines.append(line);
-}
 
-function inputValidator(inputString = input.innerText) {
-    const command = matchCommand(inputString) || inputString;
-    if (command) {
-        content[command]?.forEach((line) => {
-            newLine(line);
+        lines.append(line);
+        window.scrollTo({
+            left: 0,
+            top: document.body.scrollHeight,
+            behavior: "smooth",
         });
-    } else {
-        !input.innerText
-            ? newLine({ text: `Try 'cp --help' for more information` })
-            : newLine({
-                  text: `'${input.innerText}' is not recognized as a command`,
-              });
+    }
+
+    inputValidator(inputString = input.innerText) {
+        if (!inputString) {
+            this.newLine({ text: `TRY 'HELP' FOR MORE INFOS` });
+            return;
+        }
+
+        const command = this.matchCommand(inputString);
+        command
+            ? command.forEach((line) => this.newLine(line))
+            : this.newLine({ text: `'${inputString}' IS NOT RECOGNIZED` });
+    }
+
+    matchCommand(input) {
+        const match = Object.keys(this.content).find((command) =>
+            input.match(new RegExp(`^${command}`, "gi"))
+        );
+
+        return this.content[match];
     }
 }
 
-function matchCommand(input) {
-    return Object.keys(content).find((command) =>
-        input.match(new RegExp(`^${command}`, "gi"))
-    );
-}
-
-document.addEventListener("keydown", (event) => {
-    switch (event.code) {
-        case "Backspace": {
-            input.innerText = input.innerText.substr(
-                0,
-                input.innerText.length - 1
-            );
-            break;
-        }
-        default:
-            break;
-    }
-});
-
-document.addEventListener("keypress", (event) => {
-    switch (event.code) {
-        case "Enter": {
-            newLine({ text: `~/${input.innerText} ` });
-            inputValidator();
-            input.innerText = "";
-            break;
-        }
-        default:
-            appendInput(event.key);
-            event.preventDefault();
-            break;
-    }
-});
-
-document.addEventListener("input", (event) => {
-    if (event.data) {
-        appendInput(event.data);
-    }
-});
+new Console();
